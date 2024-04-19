595e85cf27210a2e4ba877937c27c57a99fb5c94
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-19 23:39:36 +0200

    Error Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Register a new user
 // @route   /api/users/register
 // @access  Public
-const registerUser = (req, res) => {
-    console.log(req.body);
-    res.send('Register Route');
-}
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    //return res.status(400).json({ msg: "Please enter all fields" });
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.send("Register Route");
+});
 
 // @desc    Login a user
 // @route   /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
-    res.send('Login Route');
-}
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
 
 module.exports = {
-    registerUser,
-    loginUser
-}
+  registerUser,
+  loginUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-
-app.get('/', (req, res) => {
-    //res.send('Hello World!')
-    res.status(200).json({message: "Welcome to the Support Desk API"})
-})
+app.get("/", (req, res) => {
+  //res.send('Hello World!')
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)});
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
