37835d1a78552a8ec1544fb7e5ba5ce3f7a0bb0c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-21 16:34:35 +0200

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel")
 
 // @desc    Register a new user
 // @route   /api/users/register
@@ -12,7 +15,39 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please include all fields");
   }
 
-  res.send("Register Route");
+  // Find if the user is already exists
+  const userExists = await User.findOne({ email: email });
+
+  if(userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create the user
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
+  // if user created
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      // isAdmin: user.isAdmin,
+    }) 
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  //res.send("Register Route");
 });
 
 // @desc    Login a user
