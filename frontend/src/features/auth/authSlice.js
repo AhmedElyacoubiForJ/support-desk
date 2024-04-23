63c62caf0e1d41c1999208ea126c5d3569d36f34
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-23 21:46:56 +0200

    User Login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 
 // Get user from localstorage
-const user = JSON.parse(localStorage.getItem('user'))
+const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
   user: user ? user : null,
@@ -34,14 +34,21 @@ export const register = createAsyncThunk(
 // Login user
 export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
   try {
-    console.log(user);
-  } catch (error) {}
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
 });
 
 // Logout user
 export const logout = createAsyncThunk("auth/logout", async () => {
   try {
-    await authService.logout()
+    await authService.logout();
   } catch (error) {}
 });
 
@@ -71,7 +78,21 @@ export const authSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
-      }).addCase(logout.fulfilled, (state, action) => {
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state.user = null;
+      }).addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
         state.user = null;
       });
   },
